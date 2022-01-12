6d9c12bc4300ad94012142c0b87f4a87ee9b3824
xtream2000c <carlmarcast@gmail.com>
2022-01-12 01:09:30 +0100

    Comenzado el script

--- P2/JS/script.js
+++ P2/JS/script.js
@@ -2,6 +2,15 @@ window.onload = main();
 
 function main() {
     
-    
+    alert(document.getElementById("imagen1").getAttribute("src"));
+    document.getElementById("imagen1").addEventListener("click", cambiaImagen);
+    document.getElementById("imagen2").addEventListener("click", cambiaImagen);
+    document.getElementById("imagen3").addEventListener("click", cambiaImagen);
+    document.getElementById("imagen4").addEventListener("click", cambiaImagen);
 
+}
+
+function cambiaImagen() {
+    alert(this.getAttribute("src"));
+    document.getElementsByClassName("imagen").getAttribute('src')=this.getAttribute('src');
 }
